739d22db5a608f56c3d151cbed319c83fc763c39
Renzo Crisostomo <renzo@crisostomo.me>
2019-03-16 20:55:33 +0100

    Add OpenGL 3.3 function loading

--- native-build.mk
+++ native-build.mk
@@ -5,14 +5,19 @@ BIN	:= bin
 SRC	:= src
 INCLUDE	:= include
 LIB := lib
+GLAD_BUILD_DIR := glad
 
-LIBRARIES := `sdl2-config --libs`
+LIBRARIES := `sdl2-config --libs` -ldl
 EXECUTABLE := gs-cross-compile
 
 all: $(BIN)/$(EXECUTABLE)
 
-$(BIN)/$(EXECUTABLE): $(SRC)/*.cpp
-	$(CXX) $(CXX_FLAGS) -I$(INCLUDE) -L$(LIB) $^ -o $@ $(LIBRARIES)
+$(GLAD_BUILD_DIR)/src/*.c:
+	python -m glad --out-path=$(GLAD_BUILD_DIR) --api="gl=3.3" --extensions="GL_KHR_debug" --generator="c"
+
+$(BIN)/$(EXECUTABLE): $(SRC)/*.cpp $(GLAD_BUILD_DIR)/src/*.c
+	$(CXX) $(CXX_FLAGS) -I$(INCLUDE) -I$(GLAD_BUILD_DIR)/include -L$(LIB) $^ -o $@ $(LIBRARIES)
 
 clean:
 	rm -rf $(BIN)/*
+	rm -rf $(GLAD_BUILD_DIR)/*
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <SDL2/SDL.h>
 #include <iostream>
+#include <glad/glad.h>
 using namespace std;
 
 int main() {
@@ -7,7 +8,16 @@ int main() {
         cout << "Error initializing SDL: " << SDL_GetError() << endl;
         exit(1);
     }
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
     SDL_Window *window = SDL_CreateWindow("GameShell", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 320, 240, SDL_WINDOW_OPENGL);
+    SDL_GLContext glContext = SDL_GL_CreateContext(window);
+    if (!gladLoadGLLoader((GLADloadproc) SDL_GL_GetProcAddress)) {
+        cout << "Failed to load OpenGL." << std::endl;
+        exit(1);
+    }
+    cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << endl;
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
     SDL_RenderClear(renderer);
